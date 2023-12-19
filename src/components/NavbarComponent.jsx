9eabc37d013ef6feaacba82e23f73e8affe5f56d
dhikaard <yusufardian41@gmail.com>
2023-12-19 22:30:04 +0700

    feat:Tambahkan Navbafeat: Perbarui navbar dan buat section headerr

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavbarComponent = () => {
   return (
-    <Navbar expand="lg" className="navbar navbar-light w-100 position-relative" style={{ backgroundColor: '#e3f2fd' }}>
+    <Navbar expand="lg" className="navbar w-100" style={{ backgroundColor: '#ECF5FF' }}>
     <Navbar.Brand as={Link} to="/">
       <img
         src="./src/assets/sadam.png"
@@ -13,16 +13,16 @@ const NavbarComponent = () => {
         className="d-inline-block align-top"
       />
     </Navbar.Brand>
-    <Navbar.Toggle className='navbar-toggle' aria-controls="basic-navbar-nav" />
+    <Navbar.Toggle className='navbar-toggle position-relative' aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mx-auto">
         <Nav.Link as={Link} to="/">Beranda</Nav.Link>
         <Nav.Link as={Link} to="/tentang">Tentang Sadam</Nav.Link>
         <Nav.Link as={Link} to="/pengaduan">Ajukan Pengaduan</Nav.Link>
       </Nav>
-      <div className='navbar-sign d-flex align-items-center gap-3'>
+      <div className='navbar-sign d-flex gap-3'>
         <Nav.Link as={Link} to="/link">Masuk</Nav.Link>
-        <button type="button" className="btn btn-outline-primary rounded-5">Daftar Sekarang</button>
+        <button type="button" className="btn navbar-btn btn-outline-primary rounded-5">Daftar Sekarang</button>
       </div>
     </Navbar.Collapse>
   </Navbar>  
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,41 @@
+import { Container, Row, Col } from "react-bootstrap";
+import HeroImage from '../assets/img/hero.png';
+
 const HomePage = () => {
   return (
     <div className="homepage">
-    <header className=""></header>
+    <header className="w-100 min-vh-100 d-flex">
+      <Container>
+        <Row className="header-box d-flex align-items-center">
+          <Col>
+            <h1>Jangan ragu jangan bimbang, masalahmu akan terselesaikan😱😎</h1>
+            <p>Sadam adalah solusi bagi Masyarakat yang ingin melaporkan keresahan secara mudah, cepat, dan aman tentunya. Tidak perlu datang ke instansi tujuan, cukup dari rumah saja keresahanmu bisa tersampaikan. </p>
+            <button>Laporin Keresahanmu</button>
+            <button>Daftar</button>
+          </Col>
+          <Col>
+          <img src={HeroImage} height='' alt="hero-img" />
+          </Col>
+        </Row>
+      </Container>
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div className="w-100 min-vh-100"></div>
+    <div className=""></div>
+    <div className=""></div>
+    <div className=""></div>
+    <div className=""></div>
     <div className=""></div>
     </div>
   );
