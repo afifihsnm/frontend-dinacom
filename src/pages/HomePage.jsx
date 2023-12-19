ab028f9e7d4f6ad7303050efa84fbc85ad6e5a78
dhikaard <yusufardian41@gmail.com>
2023-12-19 22:37:44 +0700

    feat:Memperbarui Navbar dibagian Responsive dan Membuat Section Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
           <Col>
             <h1>Jangan ragu jangan bimbang, masalahmu akan terselesaikan😱😎</h1>
             <p>Sadam adalah solusi bagi Masyarakat yang ingin melaporkan keresahan secara mudah, cepat, dan aman tentunya. Tidak perlu datang ke instansi tujuan, cukup dari rumah saja keresahanmu bisa tersampaikan. </p>
-            <button>Laporin Keresahanmu</button>
+            <button className="btn">Laporin Keresahanmu</button>
             <button>Daftar</button>
           </Col>
           <Col>
