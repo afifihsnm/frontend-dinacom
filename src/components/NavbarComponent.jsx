8f618d6d1e29f22d68c19b83bf7806c6d127bc0a
dhikaard <yusufardian41@gmail.com>
2023-12-21 01:20:22 +0700

    feat: Memperbaiki Navbar, Memperbarui Header, Menambahkan Wording & Text berjalan

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,31 +1,53 @@
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavbarComponent = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Navbar expand="lg" className="navbar w-100" style={{ backgroundColor: '#ECF5FF' }}>
-    <Navbar.Brand as={Link} to="/">
-      <img
-        src="./src/assets/sadam.png"
-        alt="Logo_Sadam"
-        height="44"
-        className="d-inline-block align-top"
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle className='navbar-toggle position-relative' aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mx-auto">
-        <Nav.Link as={Link} to="/">Beranda</Nav.Link>
-        <Nav.Link as={Link} to="/tentang">Tentang Sadam</Nav.Link>
-        <Nav.Link as={Link} to="/pengaduan">Ajukan Pengaduan</Nav.Link>
-      </Nav>
-      <div className='navbar-sign d-flex gap-3'>
-        <Nav.Link as={Link} to="/link">Masuk</Nav.Link>
-        <button type="button" className="btn navbar-btn btn-outline-primary rounded-5">Daftar Sekarang</button>
-      </div>
-    </Navbar.Collapse>
-  </Navbar>  
+      <Navbar.Brand as={Link} to="/">
+        <img
+          src="./src/assets/sadam.png"
+          alt="Logo_Sadam"
+          height="44"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle
+        className={`navbar-toggle position-relative ${expanded ? 'active' : ''}`}
+        onClick={handleToggle}
+        aria-controls="basic-navbar-nav"
+      >
+        <span className={`fas ${expanded ? 'fa-light fa-arrow-right-long' : 'fa-bars'}`}></span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto">
+          <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
+            Beranda
+          </Nav.Link>
+          <Nav.Link as={Link} to="/tentang" onClick={() => setExpanded(false)}>
+            Tentang Sadam
+          </Nav.Link>
+          <Nav.Link as={Link} to="/pengaduan" onClick={() => setExpanded(false)}>
+            Ajukan Pengaduan
+          </Nav.Link>
+        </Nav>
+        <div className='navbar-sign d-flex'>
+          <Nav.Link as={Link} to="/signup" onClick={() => setExpanded(false)}>
+            Masuk
+          </Nav.Link>
+          <button type="button" className="btn navbar-btn btn-outline-primary rounded-5" onClick={() => setExpanded(false)}>
+            Daftar Sekarang
+          </button>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,39 +1,60 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from '../assets/img/hero.png';
+import HeroImage2 from '../assets/img/hero2.png';
+import HeroImage3 from '../assets/img/hero3.png';
+
 
 const HomePage = () => {
   return (
     <div className="homepage">
-    <header className="w-100 min-vh-100 d-flex">
+    <header className="w-100 d-flex">
       <Container>
         <Row className="header-box d-flex align-items-center">
-          <Col>
+          <Col lg="6" className="left p-0">
             <h1>Jangan ragu jangan bimbang, masalahmu akan terselesaikan😱😎</h1>
             <p>Sadam adalah solusi bagi Masyarakat yang ingin melaporkan keresahan secara mudah, cepat, dan aman tentunya. Tidak perlu datang ke instansi tujuan, cukup dari rumah saja keresahanmu bisa tersampaikan. </p>
-            <button className="btn">Laporin Keresahanmu</button>
-            <button>Daftar</button>
+            <div className="d-flex gap-3">
+            <button type="button" className="btn report btn-primary rounded-5">
+            Laporin keresahanmu
+          </button>
+            <button type="button" className="btn sign btn-outline-primary rounded-5">
+            Daftar
+           </button>
+           </div>
           </Col>
-          <Col>
-          <img src={HeroImage} height='' alt="hero-img" />
+          <Col lg="6" className="right p-0 pt-lg-0 pt-5">
+          <img src={HeroImage} alt="hero-img" className="hero1"/>
+          <img src={HeroImage2} alt="hero-img" className="hero2" />
+          <img src={HeroImage3} alt="hero-img" className="hero3" />
           </Col>
         </Row>
       </Container>
     </header>
 
+    <div className="wording-1 w-100 mb-5">
+      <h1>Ribet? kata siapa 😡, di Sadam semua kegiatan bisa dilakukan secara online kok, engga perlu capek-capek ke kantor instansi lagi👉👈</h1>
+      <p>Di Sadam cuma perlu 3 langkah untuk menyelesaikan sebuah masalah. Pertama, buat laporan. kedua, tindak lanjut dari instansi terkait. ketiga, saling tanggap. Selesai 😁</p>
+    </div>
 
+    <div className="wording-2 w-100">
+      <h1>Masih ragu? lebih dari 200.000 masalah sudah terselesaikan dengan baik lho. Yuk, sampaikan keresahanmu sekarang.</h1>
+    </div>
 
+    <div className="marquee-container" style={{ backgroundColor: '#0E206A' }}>
+      <div className="marquee-text">
+      <p>
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span> 
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span> 
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span> 
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span>
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span> 
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span> 
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot">•</span> 
+      Sistem Pengaduan Online Masyarakat <span className="bold-dot"></span>
+    </p>
+      </div>
+   </div>
 
-
-
-
-
-
-
-
-
-    <div className="w-100 min-vh-100"></div>
-    <div className=""></div>
-    <div className=""></div>
     <div className=""></div>
     <div className=""></div>
     <div className=""></div>
